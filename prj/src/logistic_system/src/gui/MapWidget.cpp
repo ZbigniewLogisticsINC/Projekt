9f1fe2c7fb3100644eb6b469508af2d35141b547
Marcin Ochman <marcinochman1993@wp.pl>
2015-05-14 17:48:56 +0200

    Dodano rysowanie mapy

--- prj/src/logistic_system/src/gui/MapWidget.cpp
+++ prj/src/logistic_system/src/gui/MapWidget.cpp
@@ -9,16 +9,60 @@
 #include <QPainter>
 #include <QPaintEvent>
 
+const double MapWidget::WIDTH = 5.0;
+const double MapWidget::HEIGHT = 5.0;
+
 void MapWidget::paintEvent(QPaintEvent* paintEvent)
 {
-	QPalette palette;
-	palette.setColor(QPalette::Background, Qt::white);
-	setAutoFillBackground(true);
-	setPalette(palette);
+  QPalette palette;
+  palette.setColor(QPalette::Background, Qt::white);
+  setAutoFillBackground(true);
+  setPalette(palette);
 
-	QPainter painter(this);
-	painter.drawText(20, 20, tr("Map hasn't been finished yet"));
-	//todo zaimplementować mapę
+  int width = this->width(), height = this->height();
+  QPainter painter(this);
+
+  if (getRobotManagerData() == nullptr)
+  {
+    painter.drawText(20, 20, tr("No data to show"));
+    return;
+  }
+
+  QPixmap robotIcon(":/icon/robotMap.png");
+  for (const Robot& robot : getRobotManagerData()->robotVector())
+  {
+    QPixmap scaledRobotIcon = robotIcon.scaledToWidth(0.3 / WIDTH * width);
+    painter.drawPixmap(robot.WezWspY() / WIDTH * width,
+        robot.WezWspX() / HEIGHT * height, scaledRobotIcon);
+  }
+
+  QPixmap garageIcon(":/icon/garage.png");
+  for (const Garaz& garage : getRobotManagerData()->garageVector())
+  {
+    QPixmap scaledGarageIcon = garageIcon.scaledToWidth(0.3 / WIDTH * width);
+    int x = garage.WezWspY() / WIDTH * width - 0.5 * scaledGarageIcon.width(),
+        y = garage.WezWspX() / HEIGHT * height
+            - 0.5 * scaledGarageIcon.height();
+    painter.drawPixmap(x, y, scaledGarageIcon);
+    painter.drawText(x + 1.02 * scaledGarageIcon.width(),
+        y + 1.02 * scaledGarageIcon.height(),
+        QString("%1").arg(garage.WezGarazId()));
+  }
+
+  QPixmap storageIcon(":/icon/storage.png");
+  for (const Magazyn& storage : getRobotManagerData()->storeVector())
+  {
+    QPixmap scaledStorageIcon = storageIcon.scaledToWidth(0.3 / WIDTH * width);
+    int x = storage.WezWspY() / WIDTH * width - 0.5 * scaledStorageIcon.width(),
+        y = storage.WezWspX() / HEIGHT * height
+            - 0.5 * scaledStorageIcon.height();
+    painter.drawPixmap(x, y, scaledStorageIcon);
+    painter.drawText(x + 1.05 * scaledStorageIcon.width(),
+        y + 1.05 * scaledStorageIcon.height(),
+        QString("%1").arg(storage.WezMagazynId()));
+  }
+
+  QWidget::paintEvent(paintEvent);
 }
 
 void MapWidget::refreshDataViewed()
