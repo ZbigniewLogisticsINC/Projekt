cc52bfe2172c24dbef14ec1a5db4c4a425f19c12
Wojciech Sienniak <wsiennia@lab1.5-20>
2015-05-28 16:48:31 +0200

    Dzialajacy jedz_do.py

--- prj/src/logistic_system/python/jedz_do.py
+++ prj/src/logistic_system/python/jedz_do.py
@@ -17,60 +17,79 @@ x2=float(sys.argv[2])
 y2=float(sys.argv[3])
 
 def obroc(p):
+	xx=p.GetPose()
+	x1=xx[0]	#bierzaca pozycja x
+	y1=xx[1]	#bierzaca pozycja y
+	theta=xx[2]*3.1415/180	#bierzaca kat obrotu
+	print theta
+
+	if abs(x2-x1)<0.1:
+		if y2>y1:
+			print "y2>y1"			
+			kat=1.57
+		else: 
+			print "y2<y1"
+			kat=-1.57
+	else:
+		if abs(y2-y1)<0.1:
+			if x2<x1:
+				print "x2<x1"			
+				kat=3.1415
+			else: 
+				print "x2>x1"
+				kat=0
+		else:	
+			
+			if y2<y1:
+				if x2<x1:
+					print "y2<y1 x2<x1"							
+					kat=atan((y2-y1)/(x2-x1))
+					kat=kat-3.1415
+					
+				else:
+					print "y2<y1 x2>x1"	
+					kat=atan((y2-y1)/(x2-x1))
+			else:
+				if x2<x1:
+					print "y2>y1 x2<x1"								
+					kat=atan((y2-y1)/(x2-x1))
+					kat=kat+3.1415
+				else:
+					print "y2>y1 x2>x1"	
+					kat=atan((y2-y1)/(x2-x1))
+						
+
 	ustawiony = False
 	i=0
 	while (ustawiony == False):
 		xx=p.GetPose()
 		x1=xx[0]	#bierzaca pozycja x
 		y1=xx[1]	#bierzaca pozycja y
-		theta=xx[2]*3.1415/180	#bierzaca kat obrotu
-		print theta
-
-		if abs(x2-x1)<0.1:
-			if y2>y1:
-				print "y2>y1"			
-				kat=1.57
-			else: 
-				print "y2<y1"
-				kat=-1.57
-		else:
-			if abs(y2-y1)<0.1:
-				if x2<x1:
-					print "x2<x1"			
-					kat=3.1415
-				else: 
-					print "x2>x1"
-					kat=0
-			else:	
-				print "nic z pozostalych"
-				if y2<y1:		
-					kat=atan((y2-y1)/(x2-x1))	
-				else:
-					kat=atan((y2-y1)/(x2-x1))
-					kat=1.57-kat
-				if x2<x1 and y2<y1:
-					kat=atan((y2-y1)/(x2-x1))+3.1415
-				
-				
-
-		print "kat "
-		print kat
-		print "theta "
-		print theta
+		theta=xx[2]*3.1415/180	#bierzacy kat obrotu
 		if kat > theta:
 			time = kat-theta
-			p.SetSpeed(0,1,time)
-			p.SetSpeed(0,0,0)
+			if time > 3.1415:
+				time = 6.283 - time
+				p.SetSpeed(0,-1,time)
+				p.SetSpeed(0,0,0)
+			else:
+				p.SetSpeed(0,1,time)
+				p.SetSpeed(0,0,0)
 		else:
 			time = theta-kat
-			p.SetSpeed(0,-1,time)
-			p.SetSpeed(0,0,0)
+			if time > 3.1415:
+				time = 6.283 - time
+				p.SetSpeed(0,1,time)
+				p.SetSpeed(0,0,0)
+			else:
+				p.SetSpeed(0,-1,time)
+				p.SetSpeed(0,0,0)
 
 		#print p.ReadSonar()
 		xx=p.GetPose()
 		theta2=xx[2]*3.1415/180
 		print theta2
-		if (abs(theta - theta2) < 0.001):
+		if (abs(theta - theta2) < 0.0001):
 			ustawiony = True
 		p.SetSpeed(0,0,0.5)
 
@@ -86,10 +105,5 @@ def funkcja(p):
 	d=sqrt(pow(x2-x1,2)+pow(y2-y1,2))
 	p.GoTo(d)
 	
-	#p.SetSpeed(0,0,0)
-				
-
 if __name__ == '__main__':
-	
-
 	funkcja(p)
