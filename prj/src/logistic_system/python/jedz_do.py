fdb6667a1ccc4bfd9f90bac089cc8c48826cc038
WojciechSienniak <w.sienniak@gmail.com>
2015-06-17 11:20:27 +0200

    Dodane komentarze do skryptu jedz_do.py

--- prj/src/logistic_system/python/jedz_do.py
+++ prj/src/logistic_system/python/jedz_do.py
@@ -5,24 +5,50 @@ import sys
 import roslib
 import rospy
 from geometry_msgs.msg import Twist, Pose
-from drive2 import RosAriaDriver2
+from drive import RosAriaDriver
 from math import atan, sqrt, pow
 
-robot = sys.argv[1]
+'''
+Obsługa argumentów wywołania skryptu.
+
+#argument 1: numer robota (zostaje zkonkatenowany do zmiennych typu string)
+#argument 2: współrzędna x punktu celu
+#argument 3: współrzędna y punkty celu
+
+'''
+robot = sys.argv[1]	
 string1 = '/PIONIER{0}/RosAria/pose'.format(robot)
 string2 = '/PIONIER{0}/RosAria/cmd_vel'.format(robot)
-string3 = '/PIONIER{0}'.format(robot)
-p=RosAriaDriver2(string3)	
+string3 = '/PIONIER{0}'.format(robot)	
 x2=float(sys.argv[2])
 y2=float(sys.argv[3])
 
-def obroc(p):
-	xx=p.GetPose()
-	x1=xx[0]	#bierzaca pozycja x
-	y1=xx[1]	#bierzaca pozycja y
-	theta=xx[2]*3.1415/180	#bierzaca kat obrotu
-	print theta
+'''
+Stworzenie obiektu klasy RosAriaDriver.
+'''
+p=RosAriaDriver(string3)
 
+'''
+Definicja funkcja obracającej robota.
+
+#parametr: p obiekt klasy RosAriaDriver
+
+Funkcja pozwala nakierować, ustawić robota na cel. 
+
+'''
+def obroc(p):
+	xx=p.GetPose()	#Pobranie bierzącej pozycji robota oraz jego orientacji.
+	x1=xx[0]	#x - bierząca współrzędna x robota w metrach.
+	y1=xx[1]	#y - bierząca współrzędna y robota w metrach.
+	theta=xx[2]*3.1415/180	#theta - bierzący kąt obrotu robota, jego orientacja w radianach.
+	#print theta
+	
+	'''
+	Poniższa część kodu funkcji obroc dotyczny obsługi przypadków jakie mogą wystąpić, aby dobrze obliczyć orientację, 
+	kąt, na który robot powinien zostać ustawiony względem jego aktualnej pozycji. Po wykonaniu funkcji obroc 
+	kąt theta robota powinna przyjąć wartość obliczonego niżej kąta.
+	#zmienne:	kat - obliczana orientacja potrzebna do nakierowania robota na cel (orientacja końcowa).
+	'''
 	if abs(x2-x1)<0.1:
 		if y2>y1:
 			print "y2>y1"			
@@ -58,14 +84,24 @@ def obroc(p):
 					print "y2>y1 x2>x1"	
 					kat=atan((y2-y1)/(x2-x1))
 						
-
+	'''
+	Następnym etapem funkcji obroc jest ustawienie robota na obliczoną orientację.
+	Ruch robota zadawany jest za pomocą metody dostępnej w klasie RosAriaDriver SetSpeed()
+	#Zmienne:	kat - obliczona orientacja potrzebna do nakierowania robota na cel (orientacja końcowa).
+			time - odwzorowuje kąt o jaki robot powinien się obrócić  względem orientacji końcowej i aktualnej orientacji.
+			theta - orientacja robota pobierana z każdą iteracją pętli while na jej początku w radianach.
+			theta2 - orientacja robota pobierana z każdą iteracją pętli while po wykonaniu obrotu w radianach.
+			ustawiony - gdy zostaje ustawiona na true oznacza, że różnica między kątami theta oraz theta2 jest bliska 0, 
+			czyli robot osiągnął już właściwą orientację.
+			
+	'''
 	ustawiony = False
 	i=0
 	while (ustawiony == False):
-		xx=p.GetPose()
-		x1=xx[0]	#bierzaca pozycja x
-		y1=xx[1]	#bierzaca pozycja y
-		theta=xx[2]*3.1415/180	#bierzacy kat obrotu
+		xx=p.GetPose()	#Pobranie bierzącej pozycji robota oraz jego orientacji.
+		x1=xx[0]	#x - bierząca współrzędna x robota w metrach.
+		y1=xx[1]	#y - bierząca współrzędna y robota w metrach.
+		theta=xx[2]*3.1415/180	#theta - bierzący kąt obrotu robota, jego orientacja w radianach.
 		if kat > theta:
 			time = kat-theta
 			if time > 3.1415:
@@ -85,14 +121,18 @@ def obroc(p):
 				p.SetSpeed(0,-1,time)
 				p.SetSpeed(0,0,0)
 
-		#print p.ReadSonar()
 		xx=p.GetPose()
 		theta2=xx[2]*3.1415/180
 		print theta2
 		if (abs(theta - theta2) < 0.0001):
 			ustawiony = True
 		p.SetSpeed(0,0,0.5)
+'''
+Definicja funkcji wykonującej przemieszczenie robota do punktu celu.
+#parametr:	p obiekt klasy RosAriaDriver
 
+#Zmienne:	d - długość dystansu z punktu startowego do punktu celu robota liczona za pomocą wzoru na długość odcinka.
+'''
 def funkcja(p):
 	#Ustawienie się robota w kierunku celu.
 	obroc(p);
@@ -103,7 +143,8 @@ def funkcja(p):
 	x1=xx[0]
 	y1=xx[1]
 	d=sqrt(pow(x2-x1,2)+pow(y2-y1,2))
-	p.GoTo(d)
+	p.GoTo(d)	#Jazda robota na wprost przez odległość d, wykorzystuje się metodę klasy RosAriaDrier GetPose().
 	
 if __name__ == '__main__':
-	funkcja(p)
+	funkcja(p)	#Wywołanie funkcja
+
